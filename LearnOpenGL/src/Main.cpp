29f90f4dc4cc7a038fea69b179642f12304d19c1
Ray <icefoxtx@gmail.com>
2022-07-04 17:00:02 +0400

    Created first window!

--- LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/src/Main.cpp
@@ -1,7 +1,49 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <cstdio>
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+	glfwSetErrorCallback([](int errorCode, const char *description) { fprintf(stderr, "GLFW Error %i: %s\n", errorCode, description); });
+
+	if (glfwInit() == 0)
+	{
+		fprintf(stderr, "%s\n", "Failed to initialize glfw!");
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == 0)
+	{
+		fprintf(stderr, "%s\n", "Failed to create window!");
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+	{
+		fprintf(stderr, "%s\n", "Failed to initialize glad!");
+		glfwTerminate();
+		return 1;
+	}
+
+	printf("GL version: %s\n", glGetString(GL_VERSION));
+	printf("GL vendor: %s\n", glGetString(GL_VENDOR));
+
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow *window, int width, int height) { glViewport(0, 0, width, height); });
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
 }
