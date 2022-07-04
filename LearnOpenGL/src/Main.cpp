7f4589988c438274670fb45b0407a2efd5bc5fe7
Ray <icefoxtx@gmail.com>
2022-07-05 00:03:42 +0400

    Quad!

--- LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/src/Main.cpp
@@ -2,7 +2,16 @@
 #include <GLFW/glfw3.h>
 #include <cstdio>
 
-float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+float vertices[] = {
+	0.5f,  0.5f,  0.0f,	 // top right
+	0.5f,  -0.5f, 0.0f,	 // bottom right
+	-0.5f, -0.5f, 0.0f,	 // bottom left
+	-0.5f, 0.5f,  0.0f	 // top left
+};
+unsigned int indices[] = {
+	0, 1, 3,  // first triangle
+	1, 2, 3	  // second triangle
+};
 
 const char *vertexShaderSource = R"glsl(
 #version 330 core
@@ -111,13 +120,16 @@ int main()
 
 	glUseProgram(shaderProgram);
 
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glGenVertexArrays(1, &VAO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
 	glEnableVertexAttribArray(0);
 
@@ -130,7 +142,7 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		glfwSwapBuffers(window);
 	}
