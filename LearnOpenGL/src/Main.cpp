8d15b2b47003c1e23f2987a4447ed66a1b35bc71
Ray <icefoxtx@gmail.com>
2022-07-04 17:08:41 +0400

    Added clear color

--- LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/src/Main.cpp
@@ -48,6 +48,10 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 	}
 
